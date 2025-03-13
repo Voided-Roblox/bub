dbe2dc43d616d4ebb1c29dd558c031ea65d1ecf2
Voided-Roblox <riondhingra@gmail.com>
2025-03-12 20:48:17 -0700

    Created using Colab

---
+++ your name.py
@@ -0,0 +1,64 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOJePb89Hk4ze6cOQr2yEef",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Voided-Roblox/bub/blob/main/your%20name.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 12,
+      "metadata": {
+        "id": "GBAT_rBpmdk2"
+      },
+      "outputs": [],
+      "source": []
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "bub=input ('What is your bub')\n",
+        "print('Hi ', bub)"
+      ],
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "_MvHDkX_mmId",
+        "outputId": "5610eea7-596d-45fc-872a-47c400cdf151"
+      },
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "What is your bub fiuisjfhijsfjds\n",
+            "Hi   fiuisjfhijsfjds\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
